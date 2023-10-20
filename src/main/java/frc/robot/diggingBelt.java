cbc0089e8a6f1e80f58b28387e5bd7c69969a4c6
Barry Lin <tun32913@temple.edu>
2023-10-20 19:04:27 -0400

    trying diggingbelt

---
+++ src/main/java/frc/robot/diggingBelt.java
@@ -0,0 +1,33 @@
+package frc.robot;
+
+import java.util.*;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+
+
+public class diggingBelt extends SubsystemBase{
+
+
+    protected int diggingSpeed= 1100;
+    public boolean isBeltRunning=false;
+
+    
+    
+    
+    public diggingBelt(){
+        
+    }
+
+
+    public boolean diggingCondition(){
+
+        return false;
+    }
+
+
+   
+
+
+}
