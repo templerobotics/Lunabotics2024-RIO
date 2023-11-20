cf894a6ca95e2a87980be1f55d59b04cd44813c5
vAnomali <jaredpc378@gmail.com>
2023-11-20 18:42:47 -0500

    Leadscrew files

---
+++ src/main/java/frc/robot/commands/digging/ExtendLeadscrew.java
@@ -0,0 +1,32 @@
+package frc.robot.commands.digging;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Digging.DiggingLeadscrew;
+import frc.robot.subsystems.Digging.DiggingLeadscrew.LeadscrewState;
+
+public class ExtendLeadscrew extends CommandBase {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+	private final DiggingLeadscrew m_subsystem;
+
+    public ExtendLeadscrew(DiggingLeadscrew subsystem) {
+        m_subsystem = subsystem;
+        addRequirements(subsystem);
+    }
+
+    @Override
+    public void initialize() {
+        m_subsystem.leadscrew(LeadscrewState.Extended);
+    }
+
+    @Override
+    public void execute() {}
+
+    @Override 
+    public void end(boolean interrupted) {}
+
+    @Override
+    public boolean isFinished() {
+        return m_subsystem.leadscrewState() == LeadscrewState.Extended;
+    }
+
+}
