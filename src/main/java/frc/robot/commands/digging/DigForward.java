06b751bc6c3ac7363f1c49adb4e9aa25b43fc7fd
Jake Grinshpun <jake.grinshpun2003@gmail.com>
2023-12-01 10:18:24 -0500

    Created Digging Commands

---
+++ src/main/java/frc/robot/commands/digging/DigForward.java
@@ -0,0 +1,35 @@
+package frc.robot.commands.digging;
+
+import frc.robot.subsystems.digging.DiggingBelt;
+import edu.wpi.first.wpilibj2.command.CommandBase;;
+
+public class DigForward extends CommandBase {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+	private final DiggingBelt m_subsystem;
+
+    // Creates a new DigForward Object
+    // @param subsystem: THe subsystem used by this command
+    public DigForward(DiggingBelt subsystem){
+        m_subsystem = subsystem;
+        //Decalre subsystem dependencies
+        addRequirements(subsystem);
+    }
+
+    // Called every time the scheduler runs while command is scheduled
+    @Override
+    public void execute(){
+        m_subsystem.runBelt(false); //Run Belt
+    }
+
+    // Called when command ends or is interupted
+    @Override
+    public void end(boolean interupted){
+        m_subsystem.stopBelt(); //Stops belt
+    }
+
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
+    
+}
---
+++ src/main/java/frc/robot/commands/digging/DigReverse.java
@@ -0,0 +1,35 @@
+package frc.robot.commands.digging;
+
+import frc.robot.subsystems.digging.DiggingBelt;
+import edu.wpi.first.wpilibj2.command.CommandBase;;
+
+public class DigReverse extends CommandBase {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+	private final DiggingBelt m_subsystem;
+
+    // Creates a new DigReverse Object
+    // @param subsystem: THe subsystem used by this command
+    public DigReverse(DiggingBelt subsystem){
+        m_subsystem = subsystem;
+        //Decalre subsystem dependencies
+        addRequirements(subsystem);
+    }
+
+    // Called every time the scheduler runs while command is scheduled
+    @Override
+    public void execute(){
+        m_subsystem.runBelt(true); //Run Belt in reverse
+    }
+
+    // Called when command ends or is interupted
+    @Override
+    public void end(boolean interupted){
+        m_subsystem.stopBelt(); //Stops belt
+    }
+
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
+    
+}
